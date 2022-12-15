11b0fc2b445469609aea5fbda1d09a410e4d2e1a
Nikko Bansil <bjohnnikko@gmail.com>
2022-12-15 15:15:14 -0800

    refactor getAllReservations to use SQL

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -80,7 +80,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT reservations.id, properties.*, reservations.start_date
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2;`, [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getAllReservations = getAllReservations;
 
