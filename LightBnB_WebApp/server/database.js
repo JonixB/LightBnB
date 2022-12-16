b0a7d811b78e7b3dec46d541290134bd3cc89540
Nikko Bansil <bjohnnikko@gmail.com>
2022-12-15 17:30:05 -0800

    fix getAllReservations to show average rating

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -81,9 +81,10 @@ exports.addUser = addUser;
  */
 const getAllReservations = function(guest_id, limit = 10) {
   return pool
-    .query(`SELECT reservations.id, properties.*, reservations.start_date
+    .query(`SELECT reservations.id, properties.*, reservations.start_date, avg(property_reviews.rating) as average_rating
     FROM reservations
     JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
     WHERE reservations.guest_id = $1
     GROUP BY properties.id, reservations.id
     ORDER BY reservations.start_date
